05a924bd9073c15f4c79cf8809bc778755bb7f02
inbasusi8 <40240090+09Susinthan08@users.noreply.github.com>
2019-02-11 15:19:14 +0530

    Create set1_05.py

---
+++ set1_05.py
@@ -0,0 +1,44 @@
+s = input()
+s = s.upper()
+if s == 'I':
+	print('1')
+elif s == 'II':
+	print('2')
+elif s == 'III':
+	print('3')
+elif s == 'IV':
+	print('4')
+elif s == 'V':
+	print('5')
+elif s == 'VI':
+	print('6')
+elif s == 'VII':
+	print('7')
+elif s == 'VIII':
+	print('8')
+elif s == 'IX':
+	print('9')
+elif s == 'X':
+	print('10')
+elif s == 'XI':
+	print('11')
+elif s == 'XII':
+	print('12')
+elif s == 'XIII':
+	print('13')
+elif s == 'XIV':
+	print('14')
+elif s == 'XV':
+	print('15')
+elif s == 'XVI':
+	print('16')
+elif s == 'XVII':
+	print('17')
+elif s == 'XVIII':
+	print('18')
+elif s == 'XIX':
+	print('19')
+elif s == 'XX':
+	print('20')
+else:
+	()
